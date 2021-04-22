4ec73f076399da8999fee7ce047d5933d284a1ba
David Manvel Manvelyan <dmanvelyan@gmail.com>
2021-04-22 17:03:05 +0400

    Implemented opening file and reading from it

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
+func main() {
+	createreverseindex("files")
+}
+
+func createreverseindex(dir string) {
+	//ss := make(map[string][]string)
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		content, err := ioutil.ReadFile(dir + string(os.PathSeparator) + file.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%s contents: %s\n", file.Name(), content)
+	}
 }
